4b6f7f4d685d29e4ec4f463af098d954dd29b194
Carson Ritchey <carson@macbook.attlocal.net>
2022-03-03 18:52:35 -0600

    creating dir if doesn't exist

--- trash.c
+++ trash.c
@@ -2,13 +2,47 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #define NAME "trash"
-#define TRASHCAN_DIRECTORY "~/.trashcan"
+// gets concatinated onto the home dir 
+#define TRASHCAN_DIRECTORY "/.trashcan"
 #define DEFAULT_PERMS 777
 
-char* home_dir;
+// absolute path to trashcan
+char* trashcan_dir;
 
+// checks to see if trashcan exists and creates it if it doesn't
+bool create_trashcan() {
+	struct stat st;
+	if(stat(trashcan_dir, &st) == -1) {
+		printf("'%s' not found, creating directory ...\n", trashcan_dir);
+
+		mkdir(trashcan_dir, DEFAULT_PERMS);
+	}
+
+	return true;
+}
+
+bool init() {
+	trashcan_dir = getenv("HOME");
+
+	if(trashcan_dir == NULL) {
+		printf("%s: $HOME path variable not found\n", NAME);
+		return false;
+	}
+
+	strcat(trashcan_dir, TRASHCAN_DIRECTORY);
+
+	if(!create_trashcan()) {
+		printf("%s: unable to create ~%s\n", NAME, TRASHCAN_DIRECTORY);
+		return false;
+	}
+
+	return true;
+}
+
+// moves a file from one location to another
 bool move(char* source_dir, char* destination_dir) {
     FILE* source, *destination;
     source = fopen(source_dir, "r");
@@ -36,13 +70,9 @@ bool move(char* source_dir, char* destination_dir) {
 }
 
 int main(int argc, char* argv[]) {
-	home_dir = getenv("HOME");
-	if(home_dir == NULL) {
-		printf("$HOME path variable not found\nexiting ...\n");
-		return 0;
+	if(!init()) {
+		printf("%s: failed to initialize\nexiting ...\n", NAME);
 	}
-
-	printf("%s\n", home_dir);
 
     if(argc != 3) {
         printf("%s: please provide sufficient arguments\n", NAME);
